01965aa5e8b6502bd50a34fa950ae916f9a3cd69
GNarvils <gustavs.gn@gmail.com>
2023-04-03 19:01:08 +0300

    Pareja starp ainām]

---
+++ Assets/Skripti/GalvenaIzvelne.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GalvenaIzvelne : MonoBehaviour {
+		//Iet Uz Scene Pirmsspele
+		public void Pirmsspele(){
+			SceneManager.LoadScene ("Pirmsspele", LoadSceneMode.Single);
+		}
+		//Iet Uz Scene Iestatijumi
+		public void Iestatijumi(){
+			SceneManager.LoadScene ("Iestatijumi", LoadSceneMode.Single);
+		}
+	public void Izvelne(){
+		SceneManager.LoadScene ("GalvenaIzvelne", LoadSceneMode.Single);
+	}
+	public void doExitGame() {
+		Application.Quit();
+	}
+
+}
