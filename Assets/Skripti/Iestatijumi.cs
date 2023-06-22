6232b7b5bd9029e32984d3a8957e51d3881eea18
GNarvils <gustavs.gn@gmail.com>
2023-06-22 23:03:04 +0300

    Rezolucija

--- Assets/Skripti/Iestatijumi.cs
+++ Assets/Skripti/Iestatijumi.cs
@@ -9,6 +9,8 @@ public class Iestatijumi : MonoBehaviour
     public Text FOV;
     public Slider Atrums;
     public Text sens;
+    public Dropdown resolution;
+    Resolution[] resolutions;
     void Start()
     {
         FOV.text = PlayerPrefs.GetFloat("FOV") + "";
@@ -19,7 +21,31 @@ public class Iestatijumi : MonoBehaviour
         Atrums.value = PlayerPrefs.GetFloat("SensX");
         Atrums.onValueChanged.AddListener(delegate { UpdateSens(Atrums); });
 
+        resolutions = Screen.resolutions;
+        resolution.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int tagad = 0;
+
+        for (int i = 0; i < resolutions.Length; i++) {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
+                tagad = i;
+            }
+        }
+
+        resolution.AddOptions(options);
+        resolution.value = tagad;
+        resolution.RefreshShownValue();
     }
+    public void SetResolution(int tagad) {
+        Resolution resolution = resolutions[tagad];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     public void UpdateFOV(Slider FieldOFView)
     {
         FOV.text = FieldOFView.value + "";
