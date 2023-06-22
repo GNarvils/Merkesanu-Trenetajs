fb8657491f299c10602cb29abdc2b85801952834
GNarvils <gustavs.gn@gmail.com>
2023-06-22 23:23:00 +0300

    Salabota rezolūcija

--- Assets/Skripti/Iestatijumi.cs
+++ Assets/Skripti/Iestatijumi.cs
@@ -11,6 +11,8 @@ public class Iestatijumi : MonoBehaviour
     public Text sens;
     public Dropdown resolution;
     Resolution[] resolutions;
+    public List<Resolution> filtrs;
+    public float hz;
     void Start()
     {
         FOV.text = PlayerPrefs.GetFloat("FOV") + "";
@@ -23,16 +25,24 @@ public class Iestatijumi : MonoBehaviour
 
         resolutions = Screen.resolutions;
         resolution.ClearOptions();
+        filtrs = new List<Resolution>();
+        hz = Screen.currentResolution.refreshRate;
+
+        for (int i = 0; i < resolutions.Length; i++) {
+            if (resolutions[i].refreshRate == hz) {
+                filtrs.Add(resolutions[i]);
+            }
+        }
 
         List<string> options = new List<string>();
 
         int tagad = 0;
 
-        for (int i = 0; i < resolutions.Length; i++) {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+        for (int i = 0; i < filtrs.Count; i++) {
+            string option = filtrs[i].width + " x " + filtrs[i].height + " "+filtrs[i].refreshRate+ " Hz";
             options.Add(option);
 
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
+            if (filtrs[i].width == Screen.currentResolution.width && filtrs[i].height == Screen.currentResolution.height) {
                 tagad = i;
             }
         }
@@ -42,8 +52,8 @@ public class Iestatijumi : MonoBehaviour
         resolution.RefreshShownValue();
     }
     public void SetResolution(int tagad) {
-        Resolution resolution = resolutions[tagad];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        Resolution resolution = filtrs[tagad];
+        Screen.SetResolution(resolution.width, resolution.height, true);
     }
 
     public void UpdateFOV(Slider FieldOFView)
